fd0eef1b44821e3ddeabceee94df806f96696cd6
Juan Esteban Fajardo Ruiz <drummerestebanruiz@gmail.com>
2022-11-04 11:51:07 -0500

    Create context

--- src/Herores.jsx
+++ src/Herores.jsx
@@ -1,10 +1,13 @@
+import { AuthProvider } from "./auth";
 import { AppRouter } from "./router/AppRouter";
 
 export const Herores = () => {
   
   return (
-    <>
+    <AuthProvider>
+
       <AppRouter/>
-    </>  
+      
+    </AuthProvider>  
   );
 }
---
+++ src/auth/types/types.js
@@ -0,0 +1,4 @@
+const types = {
+    login:  '[Auth] Login',
+    logout: '[Auth] Logout',
+}
